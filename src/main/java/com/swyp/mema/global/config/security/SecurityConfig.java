21d79ccc48d7fbcadd4c4dfdc0905088e74487ab
choieunbi <hikarigin99@gmail.com>
2024-12-06 17:09:20 +0900

    feat: SecurityConfig Swagger 경로 허용

--- src/main/java/com/swyp/mema/global/config/security/SecurityConfig.java
+++ src/main/java/com/swyp/mema/global/config/security/SecurityConfig.java
@@ -99,7 +99,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login", "/join/custom", "/login/naver").permitAll()
-                        .anyRequest().authenticated());
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().authenticated());
 
         http
                 .sessionManagement((session) -> session
