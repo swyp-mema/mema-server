8a2a29f7be36805f6a08fca8a7a6867978cf1d38
choieunbi <hikarigin99@gmail.com>
2024-12-15 03:58:42 +0900

    feat: yml API key 값 적용

--- src/main/java/com/swyp/mema/domain/station/controller/StationController.java
+++ src/main/java/com/swyp/mema/domain/station/controller/StationController.java
@@ -15,6 +15,7 @@ import com.swyp.mema.domain.station.service.StationMasterService;
 import com.swyp.mema.domain.station.service.StationService;
 import com.swyp.mema.domain.user.dto.CustomUserDetails;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -46,7 +47,7 @@ public class StationController {
 	/**
 	 * openAPI 통해 지하철역 조회 API
 	 */
-	// @Hidden
+	@Hidden
 	@Operation(summary = "모든 지하철역 조회 API", description = "OpenAPI 로 모든 지하철역 정보를 조회합니다.")
 	@GetMapping("/meets/{meetId}/station/all")
 	public ResponseEntity<Void> getAllStationByOpenAPI(
@@ -63,7 +64,7 @@ public class StationController {
 	 * 이 API 는 무시해주세요!
 	 * openAPI 통해 지하철역 시간표 조회 API
 	 */
-	// @Hidden
+	@Hidden
 	@Operation(summary = "해당 역에 대한 모든 시간표 조회 API", description = "OpenAPI 로 해당 '역 + 호선'에 대한 모든 시간표 정보를 조회합니다.",
 		security = {})
 	@GetMapping("/station/time")
@@ -81,7 +82,7 @@ public class StationController {
 	 * 이 API 는 무시해주세요!
 	 * 지하철 실시간 시간 데이터 가져오는 API
 	 */
-	// @Hidden
+	@Hidden
 	@Operation(summary = "지하철 실시간 시간 API 통해 앞뒤 역 탐색", description = "지하철 실시간 시간 OpenAPI 로 앞뒤 역 정보를 조회합니다.",
 		security = {})
 	@GetMapping("/near/station")
@@ -98,7 +99,7 @@ public class StationController {
 	 * 이 API 는 무시해주세요!
 	 * 지하철 실시간 시간 데이터 가져오는 API
 	 */
-	// @Hidden
+	@Hidden
 	@Operation(summary = "지하철 역사 마스터 API 통해 위도 & 경도 찾기", description = "지하철 역사 마스터 OpenAPI 로 해당 역의 위도 & 경도 값을 구합니다.",
 		security = {})
 	@GetMapping("/station/master")
