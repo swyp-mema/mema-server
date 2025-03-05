4891e981dab0cadcdbdce9fb80d8842adc211d24
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-03-05 19:59:11 +0900

    chore: 역 데이터 빌드 관련 테스트 비활성화

--- src/test/java/com/swyp/mema/database/station/logic/subbuilder/StationDataBuilderServiceTest.java
+++ src/test/java/com/swyp/mema/database/station/logic/subbuilder/StationDataBuilderServiceTest.java
@@ -7,6 +7,7 @@ import com.swyp.mema.database.station.util.ExcelReader;
 import com.swyp.mema.database.station.util.StationConverter;
 import com.swyp.mema.database.station.util.StringCleaner;
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,6 +35,7 @@ public class StationDataBuilderServiceTest {
     @Autowired
     private ExcelReader excelReader;
 
+    @Disabled("위치 데이터 빌드 할 때만 테스트 하세요.")
     @Test
     public void locationBuild_Success_Test() {
 
@@ -44,6 +46,7 @@ public class StationDataBuilderServiceTest {
         assert list.isEmpty();
     }
 
+    @Disabled("위치 데이터 빌드 할 때만 테스트 하세요.")
     @Test
     public void dataMatch_Api_Test(){
 
@@ -56,6 +59,7 @@ public class StationDataBuilderServiceTest {
         list.forEach(s -> System.out.println(stringCleaner.createCode(s)));
     }
 
+    @Disabled("위치 데이터 빌드 할 때만 테스트 하세요.")
     @Test
     public void dataMatch_excel_Test(){
 
